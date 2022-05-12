085c21d0da2c1f8b9d404546e68d8ef959d7b8fb
THEO MICALETTI <theo.micaletti@icloud.com>
2022-05-12 16:10:44 +0200

    d

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,10 +13,6 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <main className="min-h-screen bg-zinc-600">
-          <Home />
-        
-        </main>
         <main className="min-h-screen">
           <Routes>
             <Route path="/" element={<Home />} />
