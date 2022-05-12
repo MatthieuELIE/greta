b122c8c6a09280234b1179541ca993c83263f5d8
THEO MICALETTI <theo.micaletti@icloud.com>
2022-05-12 15:21:53 +0200

    wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import Home from "@pages/Home";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from "@components/Navbar";
+import React from "react";
 
 import "./App.css";
 import "./index.css";
@@ -9,10 +11,14 @@ import Profils from "@pages/Profils";
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <main className="min-h-screen">
-        <Home />
-      </main>
+      <BrowserRouter>
+        <Navbar />
+        <main className="min-h-screen">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
     </div>
   );
 }
