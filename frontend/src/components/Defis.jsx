980f0292509de705b72c8fbbcf2b342d9fbefe46
Marie Clémensat <marie@MacBook-de-Marie.local>
2022-05-12 14:32:16 +0200

    compteur v1

--- frontend/src/components/Defis.jsx
+++ frontend/src/components/Defis.jsx
@@ -8,9 +8,13 @@ import {
 } from "../services/api";
 
 export default function Defis() {
+  const [count, setCount] = useState(0);
   const [weekly, setWeekly] = useState([]);
+  const [weeklyChecked, setWeeklyChecked] = useState(false);
   const [dailies, setDailies] = useState([]);
+  const [dailiesChecked, setDailiesChecked] = useState(false);
   const [randoms, setRandoms] = useState([]);
+  const [randomsChecked, setRandomsChecked] = useState(false);
 
   useEffect(() => {
     setWeekly(getRandomResources(weeklyQuest, 1));
@@ -18,18 +22,50 @@ export default function Defis() {
     setRandoms(getRandomResources(randomQuest, 5));
   }, []);
 
+  const handleDoneQuest = () => {
+    if (!weeklyChecked === true) {
+      setCount((count) => count + 70);
+    }
+    if (!weeklyChecked === false) {
+      setCount((count) => count - 70);
+    }
+  };
+
+  const handleDoneDailies = () => {
+    if (!dailiesChecked === true) {
+      setCount((count) => count + 10);
+    }
+    if (!dailiesChecked === false) {
+      setCount((count) => count - 10);
+    }
+  };
+
+  const handleDoneRandoms = () => {
+    if (!randomsChecked === true) {
+      setCount((count) => count + 5);
+    }
+    if (!randomsChecked === false) {
+      setCount((count) => count - 5);
+    }
+  };
+
   return (
     <>
+      <h1 className="text-3xl my-8">
+        <div>{count} g de CO2 économisés</div>
+      </h1>
       <h2 className="text-2xl">Défi de la semaine</h2>
       <div className="flex justify-center m-4">
         <div>
           {weekly.map((quest) => (
-            <div className="form-check">
+            <div key={weekly.id} className="form-check">
               <input
                 className="form-check-input h-4 w-4 rounded-sm transition duration-200 mt-1 align-top mr-2 cursor-pointer"
                 type="checkbox"
                 value=""
                 id={`flex${quest}`}
+                onClick={() => setWeeklyChecked(!weeklyChecked)}
+                onChange={handleDoneQuest}
               />
               <label
                 className="form-check-label inline-block text-gray-800"
@@ -45,12 +81,14 @@ export default function Defis() {
       <div className="flex justify-center m-4">
         <div>
           {dailies.map((quest) => (
-            <div className="form-check">
+            <div key={dailies.id} className="form-check">
               <input
                 className="form-check-input h-4 w-4 rounded-sm transition duration-200 mt-1 align-top mr-2 cursor-pointer"
                 type="checkbox"
                 value=""
                 id={`flex${quest}`}
+                onClick={() => setDailiesChecked(!dailiesChecked)}
+                onChange={handleDoneDailies}
               />
               <label
                 className="form-check-label inline-block text-gray-800"
@@ -66,12 +104,14 @@ export default function Defis() {
       <div className="flex justify-center m-4">
         <div>
           {randoms.map((quest) => (
-            <div className="form-check">
+            <div key={randoms.id} className="form-check">
               <input
                 className="form-check-input h-4 w-4 rounded-sm transition duration-200 mt-1 align-top mr-2 cursor-pointer"
                 type="checkbox"
                 value=""
                 id={`flex${quest}`}
+                onClick={() => setRandomsChecked(!randomsChecked)}
+                onChange={handleDoneRandoms}
               />
               <label
                 className="form-check-label inline-block text-gray-800"
