a69d1ee62ffab64106c68463001274b207ff117d
simon <simon.thore@hotmail.fr>
2022-05-12 12:17:01 +0200

    Base Profils

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import Home from "@pages/Home";
-
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import "./App.css";
+
+import Profils from "@pages/Profils";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Profils" element={<Profils />} />
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ frontend/src/pages/Profils.jsx
@@ -0,0 +1,19 @@
+import react from "react";
+import "../App.css";
+
+export default function Profils() {
+  return (
+    <div className="ProfilsPage">
+         <img src="./assets.nature.jpg " alt="" />  
+         <h1>Profils page</h1>
+         <div className="userInfo">
+      
+      <h2>users</h2>
+      <p>Jean-Eude</p>
+      <img src="./assets.avatar.jpg" alt="" />
+      </div>
+      <div className="Graph"></div>
+     <h1>Co2 économisé</h1>
+    </div>
+  );
+}
