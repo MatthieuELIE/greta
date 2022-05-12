b5444420b8576a01b4074c0125c51f7cb082777e
simon <simon.thore@hotmail.fr>
2022-05-12 15:40:01 +0200

    ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import Home from "@pages/Home";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Navbar from "@components/Navbar";
-<<<<<<< HEAD
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-=======
 import React from "react";
->>>>>>> 433bd89d0ab644cb1e4b21d48a1e44aa923649c4
 
 import "./App.css";
 import "./index.css";
@@ -17,21 +14,18 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
-<<<<<<< HEAD
         {/* <main className="min-h-screen bg-zinc-600"> */}
           {/* <Home /> */}
           <Routes>
             <Route path="/Profils" element={<Profils />} />
           </Routes>
         {/* </main> */}
-=======
         <main className="min-h-screen">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/profil" element={<Profils />} />
           </Routes>
         </main>
->>>>>>> 433bd89d0ab644cb1e4b21d48a1e44aa923649c4
       </BrowserRouter>
     </div>
   );
