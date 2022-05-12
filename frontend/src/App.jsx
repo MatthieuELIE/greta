961cd6455ca51f2c5b539e797143c2e6581c8703
Matthieu ELIE <elie.matthieu2@gmail.com>
2022-05-12 18:19:45 +0200

    achievements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "@pages/Home";
 import Navbar from "@components/Navbar";
 import FriendsBoard from "@pages/FriendsBoard";
+import AchievementsPage from "@pages/AchievementsPage";
 
 import "./App.css";
 import "./index.css";
@@ -17,7 +18,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/friendsboard" element={<FriendsBoard />} />
-
+            <Route path="/achievements" element={<AchievementsPage />} />
             <Route path="/profil" element={<Profils />} />
           </Routes>
         </main>
