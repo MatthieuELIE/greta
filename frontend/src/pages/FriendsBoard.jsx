280b17b5cf3af3d1533c81b202d99c7d11047c7c
Matthieu ELIE <elie.matthieu2@gmail.com>
2022-05-12 16:56:32 +0200

    Merging

--- frontend/src/pages/FriendsBoard.jsx
+++ frontend/src/pages/FriendsBoard.jsx
@@ -1,9 +1,59 @@
 import React from "react";
+import FriendCard from "@components/FriendCard";
 
 export default function FriendBoard() {
+  const users = [
+    {
+      id: 0,
+      name: "Milorad Yura",
+      image:
+        "https://avataaars.io/?avatarStyle=Circle&topType=ShortHairShaggyMullet&accessoriesType=Sunglasses&hairColor=BlondeGolden&facialHairType=MoustacheFancy&facialHairColor=Blonde&clotheType=ShirtCrewNeck&clotheColor=Blue01&eyeType=Close&eyebrowType=Default&mouthType=Serious&skinColor=Pale",
+      quetes: 15,
+      co2: 97,
+    },
+    {
+      id: 1,
+      name: "Badri Kazimir",
+      image:
+        "https://avataaars.io/?avatarStyle=Circle&topType=Turban&accessoriesType=Sunglasses&hatColor=White&facialHairType=BeardMajestic&facialHairColor=BlondeGolden&clotheType=ShirtScoopNeck&clotheColor=Pink&eyeType=Close&eyebrowType=UnibrowNatural&mouthType=Serious&skinColor=Brown",
+      quetes: 25,
+      co2: 237,
+    },
+    {
+      id: 2,
+      name: "Eugenius Danilo",
+      image:
+        "https://avataaars.io/?avatarStyle=Circle&topType=Eyepatch&accessoriesType=Blank&hairColor=Red&facialHairType=MoustacheFancy&facialHairColor=Brown&clotheType=ShirtCrewNeck&clotheColor=Pink&eyeType=EyeRoll&eyebrowType=RaisedExcitedNatural&mouthType=Serious&skinColor=Light",
+      quetes: 12,
+      co2: 123,
+    },
+    {
+      id: 3,
+      name: "Jaylen Venera",
+      image:
+        "https://avataaars.io/?avatarStyle=Circle&topType=Hat&accessoriesType=Sunglasses&hatColor=Heather&facialHairType=MoustacheFancy&facialHairColor=Red&clotheType=GraphicShirt&clotheColor=PastelBlue&graphicType=Selena&eyeType=Squint&eyebrowType=RaisedExcitedNatural&mouthType=Twinkle&skinColor=DarkBrown",
+      quetes: 2,
+      co2: 4,
+    },
+  ];
+
+  console.log(users);
   return (
     <div>
-      <h1>Friend' ScoreBoard</h1>
+      <h1 className="my-8 text-center font-bold text-6xl">
+        Friend' ScoreBoard
+      </h1>
+      <div className="flex flex-col w-screen items-center justify-center">
+        {users.map((user) => (
+          <FriendCard
+            key={user.id}
+            name={user.name}
+            image={user.image}
+            quetes={user.quetes}
+            co2={user.co2}
+          />
+        ))}
+      </div>
     </div>
   );
 }
