4ccf90048928b78e1bdd2a648302ebbe124c42d6
Marie Clémensat <marie@MacBook-de-Marie.local>
2022-05-12 18:04:10 +0200

    login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,15 @@
-export default function Login () {
-    return (
-        <>
-        coucou
-        </>
-    )
+export default function Login() {
+  return (
+    <>
+      <div className="absolute top-4 right-6 hover:scale-105 duration-200">
+        <button
+          id="login"
+          type="button"
+        >
+          <img src="./src/assets/avatar.png" width="60" className="ml-2 rounded-full inline"/>
+
+        </button>
+      </div>
+    </>
+  );
 }
